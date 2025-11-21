73c0a3955f2436e869c2f38f96ed64a57d1cc527
Ashish mehta <ashishmehtawork108@gmail.com>
2025-11-21 23:18:06 +0530

    semifinal

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,23 +1,11 @@
 import { db } from "@/lib/db";
-import { usersTable } from "@/lib/db/schema";
 import { NextResponse } from "next/server";
 
 export async function POST(req:Request){
-    const [newuser]=await db.insert(usersTable).values({
-        age:21,
-        email:"ashish1@gmail.com",
-        name:"ashish1"
-    }).returning({
-        name:usersTable.name
-    })
-    if(!newuser){
-        return NextResponse.json({
-            message:"user not created"
-        })
-    }
+    
     return NextResponse.json({
         message:"working",
-        data:newuser
+    
     },{
         status:201
     })
