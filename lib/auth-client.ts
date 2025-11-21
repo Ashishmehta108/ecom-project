083c93a6d893a71fb045bbb933346096de953b1f
Ashish mehta <ashishmehtawork108@gmail.com>
2025-11-22 00:41:30 +0530

    add

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
+const baseURL=process.env.NEXT_PUBLIC_APP_URL
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: `${baseURL}/api/auth`,
   fetchOptions: {
     credentials: "include",
   },
