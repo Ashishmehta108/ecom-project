0c6e261c89eb5606f9dffd7f82ef68562f6ad144
Ashish mehta <ashishmehtawork108@gmail.com>
2025-11-21 23:20:28 +0530

    semifinal

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,16 @@
 import LoginForm from "@/components/LoginForm";
 // import { isAuthenticated } from "@/server";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 export default async function Login() {
   // if (isAuthenticated) {
   //   redirect("/");
   // }
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
-
 
 //tpyvOVIzJrVsNa40GS31FJLaifOBlTVk
